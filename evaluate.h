33efc85a70ae9b93e363c463acb778ae85438867
Lorenzo125 <55649413+Lorenzo125@users.noreply.github.com>
2019-09-27 21:40:13 +0200

    Delete evaluate.h

--- evaluate.h
+++
@@ -1,9 +0,0 @@
-double evaluate (struct chrom *chr){
-  int i,m;
-  double chi=0;
-  for (m=0;m<ngene;m++)
-  f.SetParameter (m, chr->gene[m]);
-  for (i=1;i<=BIN;i++)
-  chi+=TMath::Power((f.Eval(h_noise.GetBinCenter(i),0,0,0)-h_noise.GetBinContent(i))/h_noise.GetBinError(i),2)/(BIN-ngene);
-  return chi;
-}
