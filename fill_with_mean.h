ebe08143634a0263731fc6730148d56e28715d23
Lorenzo125 <55649413+Lorenzo125@users.noreply.github.com>
2019-10-13 16:43:58 +0200

    Delete fill_with_mean.h

--- fill_with_mean.h
+++
@@ -1,9 +0,0 @@
-// costruisce l'istogramma che traccia il valore medio del chi
-
-void fill_with_mean (int num_iteraz, int num_chrom){
-  double sum = 0.;
-  for (int i=0; i<num_chrom; i++) {
-    sum+=evaluate(&pop[i]);
-  }
-  hmean.Fill(num_iteraz,sum/num_chrom);
-}
