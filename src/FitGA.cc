cfb6995328ff75763b76c1fb1e7485a5c75392f7
Lorenzo125 <55649413+Lorenzo125@users.noreply.github.com>
2020-09-02 09:52:14 +0200

    Update FitGA.cc

--- src/FitGA.cc
+++ src/FitGA.cc
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
    TH1F * aux = (TH1F *)f->Get("data");
    //
 
-   // clone the initial data in order to normalize the histogram
+   // clone the initial data and normalize the histogram
    TH1F *data = (TH1F *)aux->Clone();
    if (data->GetSumw2N() == 0) data->Sumw2(kTRUE);
    data->Scale(1 / (data->GetEntries() * data->GetBinWidth(1)));
